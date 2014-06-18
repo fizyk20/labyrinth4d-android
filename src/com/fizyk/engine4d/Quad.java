bb4564353586ff57e47c94aa64dfc254cd005c43
Bartłomiej Kamiński <fizyk20@gmail.com>
2014-06-18 17:30:47 +0200

    Implemented Quad.intersect()

--- src/com/fizyk/engine4d/Quad.java
+++ src/com/fizyk/engine4d/Quad.java
@@ -1,5 +1,7 @@
 package com.fizyk.engine4d;
 
+import java.util.Vector;
+
 import com.fizyk.math4d.Hyperplane;
 import com.fizyk.math4d.Vector4;
 
@@ -25,6 +27,15 @@ public class Quad implements Primitive {
 		v[3] = new Vertex(pos4);
 	}
 	
+	public Quad(Vertex v1, Vertex v2, Vertex v3, Vertex v4)
+	{
+		v = new Vertex[4];
+		v[0] = v1;
+		v[1] = v2;
+		v[2] = v3;
+		v[3] = v4;
+	}
+	
 	@Override
 	public void draw() {
 		// TODO Auto-generated method stub
@@ -44,8 +55,62 @@ public class Quad implements Primitive {
 	}
 
 	@Override
-	public Primitive intersect(Hyperplane h) {
-		// TODO Auto-generated method stub
+	public Primitive intersect(Hyperplane h)
+	{
+		// first we find all non-null intersections of the sides
+		Vector<Primitive> p = new Vector<Primitive>();
+		
+		Primitive tmp = new Line(v[0], v[1]).intersect(h);
+		if(tmp != null) p.add(tmp);
+		
+		tmp = new Line(v[1], v[2]).intersect(h);
+		if(tmp != null) p.add(tmp);
+		
+		tmp = new Line(v[2], v[3]).intersect(h);
+		if(tmp != null) p.add(tmp);
+		
+		tmp = new Line(v[3], v[0]).intersect(h);
+		if(tmp != null) p.add(tmp);
+		
+		// the possible results are:
+		// * nothing (0 non-null results)
+		// * point (2 Point results, equal)
+		// * line (2 non-equal Points or Line + 2 Points)
+		// * quad (4 Lines)
+		
+		if(p.size() == 0)
+			return null;
+		
+		if(p.size() == 4)
+			return this;
+		
+		if(p.size() == 2)
+		{
+			// should be 2 points
+			if(p.get(0).getClass() != Point.class || p.get(1).getClass() != Point.class)
+				return null;
+			
+			Point pt1 = (Point) p.get(0);
+			Point pt2 = (Point) p.get(1);
+			
+			// if the points are actually one point, return a point
+			if(pt1.vertex(0).pos == pt2.vertex(0).pos)
+				return pt1;
+			
+			// else, they denote a line - return this line
+			return new Line(pt1.vertex(0), pt2.vertex(0));
+		}
+		
+		// we have 3 intersections - but better check
+		if(p.size() != 3)
+			return null;
+		
+		// 3 intersections = 1 Line + 2 Points - the intersection is actually the line
+		for(int i = 0; i < 3; i++)
+			if(p.get(i).getClass() == Line.class)
+				return p.get(i);
+		
+		// we should never get here, but leave this just in case
 		return null;
 	}
 
