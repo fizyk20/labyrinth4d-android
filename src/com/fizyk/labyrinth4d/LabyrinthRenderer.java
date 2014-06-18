01d4d8ec2e7c76475ac90b80d663b9223307a366
Bartłomiej Kamiński <fizyk20@gmail.com>
2014-06-18 20:01:00 +0200

    Experimental rendering code.

--- src/com/fizyk/labyrinth4d/LabyrinthRenderer.java
+++ src/com/fizyk/labyrinth4d/LabyrinthRenderer.java
@@ -3,27 +3,39 @@ package com.fizyk.labyrinth4d;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-import android.opengl.GLES20;
+import com.fizyk.engine4d.Renderer;
+import com.fizyk.math4d.Vector4;
+
 import android.opengl.GLSurfaceView;
 
 public class LabyrinthRenderer implements GLSurfaceView.Renderer {
-
-	@Override
-	public void onSurfaceCreated(GL10 arg0, EGLConfig arg1) 
+	
+	private Renderer graph4d;
+	
+	public LabyrinthRenderer()
 	{
-		GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);	
+		graph4d = new Renderer();
 	}
 
 	@Override
-	public void onSurfaceChanged(GL10 arg0, int width, int height)
+	public void onSurfaceCreated(GL10 gl, EGLConfig arg1) 
 	{
-		GLES20.glViewport(0, 0, width, height);
+		gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);	
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height)
+	{
+		graph4d.setupProjection(gl, width, height);
 	}
 	
 	@Override
-	public void onDrawFrame(GL10 arg0)
+	public void onDrawFrame(GL10 gl)
 	{
-		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		graph4d.translate(new Vector4(0., 0., -5., 0.));
+		graph4d.cube(2.0);
+		graph4d.render(gl);
 	}
 
 }
